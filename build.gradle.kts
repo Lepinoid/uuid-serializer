d8fcf7140e88882e47fc9818ecd3fdc4d6f8024d
turtton <watagame@vivaldi.net>
2022-04-09 20:43:02 +0900

    :arrow_up: update serialization plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("multiplatform") version "1.6.20"
-    kotlin("plugin.serialization") version "1.5.20"
+    kotlin("plugin.serialization") version "1.6.20"
     `maven-publish`
 }
 
